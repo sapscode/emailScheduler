3b055fe3481be7e93c9e57b23cf123b470360be5
saps-git <saptarshi0009@gmail.com>
2022-12-18 13:32:07 +0530

    setup & integration

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const app = express();
 
 const scheduler = require("node-cron");
+const { emailOptions, transporter } = require("./services/email");
 const { PORT } = require("./config");
 
-scheduler.schedule("* * * * *", () => {
-	console.log("ola");
+scheduler.schedule("* * * * * *", () => {
+  console.log("sending mail every second");
+	transporter.sendMail(emailOptions, (err, info) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("email sent" + info.response);
+		}
+	});
 });
 
 app.listen(process.env.PORT, () => {
--- config/index.js
+++ config/index.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-	PORT: process.env.PORT
+	PORT: process.env.PORT,
+	SMTP_PORT: process.env.SMTP_PORT,
+	HOST_SERVICE: process.env.HOST_SERVICE,
+	USER_EMAIL: process.env.USER_EMAIL,
+	USER_PASSWORD: process.env.USER_PASSWORD,
+	SENDERS_EMAIL: process.env.SENDERS_EMAIL
 };
